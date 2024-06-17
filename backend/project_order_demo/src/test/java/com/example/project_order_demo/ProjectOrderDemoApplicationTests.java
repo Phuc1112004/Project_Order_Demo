1cccc6912c6cbfa6d7f6a69f488326a3bf466735
Phuc <phuctv1112004@gmail.com>
2024-06-17 09:15:39 +0700

    commit

---
+++ backend/project_order_demo/src/test/java/com/example/project_order_demo/ProjectOrderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_order_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOrderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
